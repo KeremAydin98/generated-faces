3938ccddc7f487e7849fb14b53712b3f1d69593e
KeremAydin98 <aydinke16@itu.edu.tr>
2023-08-01 23:57:39 +0300

    models completed

--- models.py
+++ models.py
@@ -18,6 +18,10 @@ class Sampling(tf.keras.layers.Layer):
     
 def create_encoder(IMAGE_SIZE, EMBEDDING_DIM):
 
+    """
+    Encoder converts the images into latent vectors(embeddings)
+    """
+
     encoder_input = tf.keras.layers.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3), name='encoder_input')
 
     x = tf.keras.layers.Conv2D(32, (3,3), strides=2, activation='relu', padding='same')(encoder_input)
@@ -38,6 +42,9 @@ def create_encoder(IMAGE_SIZE, EMBEDDING_DIM):
 
 def create_decoder(EMBEDDING_DIM, shape_before_flattening):
 
+    """
+    Decoder draws an image from a latent vector(embedding)
+    """
 
     decoder_input = tf.keras.layers.Input(shape=(EMBEDDING_DIM,), name='decoder_input')
 
@@ -53,5 +60,73 @@ def create_decoder(EMBEDDING_DIM, shape_before_flattening):
 
     return tf.keras.Model(decoder_input, decoder_output, name='decoder')
 
+
+class VAE(tf.keras.Model):
+
+    def __init__(self, encoder, decoder, **kwargs):
+
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder 
+        self.total_loss_tracker = tf.keras.metrics.Mean(name = 'total_loss')
+        self.reconstruction_loss_tracker = tf.keras.metrics.Mean(name = 'reconstruction_loss')
+        self.kl_loss_tracker = tf.keras.metrics.Mean(name = 'kl_loss')
+
+        self.BETA = 2000
+
+        self.mse = tf.keras.losses.MeanSquaredError()
+
+    @property
+    def metrics(self):
+
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker
+        ]
+    
+    def call(self, inputs):
+
+        """
+        Call the model on a particular input
+        """
+        z_mean, z_log_var, z = self.encoder(inputs)
+
+        reconstruction = self.decoder(z)
+
+        return z_mean, z_log_var, reconstruction
+
+    def train_step(self, data):
+
+        """
+        Step run during training
+        """
+
+        with tf.GradientTape() as tape:
+
+            z_mean, z_log_var, reconstruction = self(data)
+
+            # Reconstruction loss
+            reconstruction_loss = tf.reduce_mean(self.BETA * self.mse(data, reconstruction))
+
+            # KL divergence loss
+            kl_loss = tf.reduce_mean(-0.5 * tf.reduce_sum(1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var), axis=1))
+
+            # Total loss
+            total_loss = reconstruction_loss + kl_loss
+
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+
+
+        return {
+            'loss': self.total_loss_tracker.result(),
+            'reconstruction_loss': self.reconstruction_loss_tracker.result(),
+            'kl_loss': self.kl_loss_tracker.result()
+        }
 
 
